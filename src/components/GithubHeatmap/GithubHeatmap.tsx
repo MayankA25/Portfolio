437189227e680f441205e7fbe2a35bf07f4466ea
Mayank <mayank.a125052@gmail.com>
2026-07-17 23:58:26 +0530

    light theme changes

--- src/components/GithubHeatmap/GithubHeatmap.tsx
+++ src/components/GithubHeatmap/GithubHeatmap.tsx
@@ -24,12 +24,9 @@ export default function GithubHeatMap() {
   //   "#39d353",
   // ];
 
-
   return (
     <div className="flex flex-col justify-center gap-5">
-      <h1 className="text-3xl font-extrabold">
-        Contributions
-      </h1>
+      <h1 className="text-3xl font-extrabold">Contributions</h1>
       <div className="flex items-center justify-center">
         <GitHubCalendar
           username="MayankA25"
@@ -37,16 +34,17 @@ export default function GithubHeatMap() {
           blockMargin={9}
           fontSize={12}
           theme={{
+            // light: ["#dddddd", "#999999", "#666666", "#444444", "#1a1a1a"],
            light: [
-  "#dddddd",
-  "#999999",
-  "#666666",
-  "#444444",
-  "#1a1a1a",
+  "#d9d9d9",
+  "#b8b8b8",
+  "#8c8c8c",
+  "#5c5c5c",
+  "#1f1f1f",
 ],
             dark: ["#222222", "#4a4a4a", "#707070", "#a0a0a0", "#f5f5f5"],
           }}
-          colorScheme={ resolvedTheme == "dark" ? "dark" : "light" }
+          colorScheme={resolvedTheme == "dark" ? "dark" : "light"}
         ></GitHubCalendar>
       </div>
     </div>
